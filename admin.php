b4ebb1d3192a1556a90760a3bfa07abaea37bd13
ahhhhmoses <140169181+moses-ramoeletsi@users.noreply.github.com>
2024-04-08 03:50:43 +0200

    added admin page

---
+++ admin.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Amin Panel</title>
+</head>
+<body>
+    <h2>Welcom to VUS admin</h2>
+    <div class="logout">
+        <a href="logout.php">Log out</a>
+        <a href="add_new_user.php">Create User</a>
+        <a href="view_all_users.php" class="btn">View All Users</a>
+        <a href="" class="btn">Delete User Requests</a>
+    </div>
+
+</body>
+</html>
